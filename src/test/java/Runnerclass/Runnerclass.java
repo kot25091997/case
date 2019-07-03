5d6641550586424a7ad7bbb770d690bab617c47d
kot25091997 <52442490+kot25091997@users.noreply.github.com>
2019-07-03 16:38:02 +0530

    Update Runnerclass.java

--- src/test/java/Runnerclass/Runnerclass.java
+++ src/test/java/Runnerclass/Runnerclass.java
@@ -8,8 +8,8 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features={"src/main/resources/case4.feature"},
 		glue = "Stepdefinition",
-		plugin="html:target/htmlreport")
-        //plugin="json:target/cucumber.json",
+		//plugin="json:target/jsonreport")
+        plugin="json:target/cucumber.json",
        // tags={"@smoke"})
 //tags={"~@smoke"})
 //tags={"@smoke","@regression"}) 
